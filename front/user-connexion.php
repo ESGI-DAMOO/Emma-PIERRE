c8002ea536f4165e5b2870dd6186c7da4290ca6f
Antoine <falgitoine@gmail.com>
2023-07-17 17:05:05 +0200

    creation du fichier user-connexion.php pour l'interface de connexion client

---
+++ front/user-connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page de connexion</title>
+</head>
+<body>
+    <form method="post" action="verifier.php">
+        <label for="username">Nom d'utilisateur:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Mot de passe:</label><br>
+        <input type="password" id="password" name="password"><br>
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
