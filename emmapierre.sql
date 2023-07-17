d877bc98e158ab09142da93b4a2ce6ec574a46a0
Olivier PERDRIX <operdrix@myges.fr>
2023-07-17 21:33:36 +0200

    Update SQL

--- emmapierre.sql
+++ emmapierre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 juil. 2023 à 14:40
+-- Généré le : lun. 17 juil. 2023 à 19:32
 -- Version du serveur : 5.7.36
 -- Version de PHP : 8.2.3
 
@@ -31,12 +31,38 @@ DROP TABLE IF EXISTS `adresse`;
 CREATE TABLE IF NOT EXISTS `adresse` (
   `id_adresse` int(11) NOT NULL AUTO_INCREMENT,
   `adresse` text NOT NULL,
-  `complement` text NOT NULL,
+  `complement` text,
   `code_postal` varchar(5) NOT NULL,
   `ville` varchar(100) NOT NULL,
   `pays` varchar(50) NOT NULL,
   PRIMARY KEY (`id_adresse`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `adresse`
+--
+
+INSERT INTO `adresse` (`id_adresse`, `adresse`, `complement`, `code_postal`, `ville`, `pays`) VALUES
+(1, '114 Enim. Ave', NULL, '43449', 'Hinckley', 'France'),
+(2, 'Ap #611-8802 Proin Ave', NULL, '96582', 'Cajazeiras', 'France'),
+(3, 'P.O. Box 193, 9298 Sit Street', NULL, '82982', 'Caruaru', 'France'),
+(4, '5107 Cras Av.', NULL, '48632', 'Gyeongsan', 'France'),
+(5, '966-5897 Ut Ave', NULL, '86216', 'Vereeniging', 'France'),
+(6, '201-2773 Arcu. Avenue', NULL, '25926', 'Brønnøysund', 'France'),
+(7, '2364 Donec Rd.', NULL, '62354', 'Kimberley', 'France'),
+(8, '2077 Tellus Rd.', NULL, '55666', 'Cannole', 'France'),
+(9, '7735 A, Rd.', NULL, '79947', 'Beijing', 'France'),
+(10, '183-8095 Magna. Ave', NULL, '88647', 'Jeju', 'France'),
+(11, 'Ap #551-3425 Lectus Street', NULL, '35717', 'Périgueux', 'France'),
+(12, '835-436 Nullam Road', NULL, '81441', 'Certaldo', 'France'),
+(13, '653-9378 Orci, St.', NULL, '83313', 'Donetsk', 'France'),
+(14, '820-8245 Nulla Road', NULL, '09430', 'Edmundston', 'France'),
+(15, 'Ap #770-7875 Varius Av.', NULL, '17365', 'Perth', 'France'),
+(16, 'Ap #795-3120 Vulputate, St.', NULL, '90523', 'Oviedo', 'France'),
+(17, 'Ap #820-6324 Odio Avenue', NULL, '77259', 'Jaén', 'France'),
+(18, '345-5775 Rhoncus. Road', NULL, '34581', 'Wageningen', 'France'),
+(19, '315-1068 Molestie Street', NULL, '77363', 'Iqaluit', 'France'),
+(20, '5296 Et Avenue', NULL, '96710', 'Guysborough', 'France');
 
 -- --------------------------------------------------------
 
@@ -75,6 +101,16 @@ CREATE TABLE IF NOT EXISTS `code_promo` (
   PRIMARY KEY (`id_code_promo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `code_promo`
+--
+
+INSERT INTO `code_promo` (`id_code_promo`, `remise`) VALUES
+('20SUR20', 20),
+('JOYEUXANNIVMAXIME', 50),
+('LOVEJS', 30),
+('LOVEPHP', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -85,9 +121,10 @@ DROP TABLE IF EXISTS `commande`;
 CREATE TABLE IF NOT EXISTS `commande` (
   `id_commande` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
-  `id_code_promo` varchar(50) NOT NULL,
-  `date_commande` date NOT NULL,
-  `date_reglement` date NOT NULL,
+  `id_code_promo` varchar(50) DEFAULT NULL,
+  `date_commande` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_reglement` date DEFAULT NULL,
+  `id_statut` int(11) NOT NULL,
   `id_adresse_livraison` int(11) NOT NULL,
   `id_adresse_facturation` int(11) NOT NULL,
   PRIMARY KEY (`id_commande`),
@@ -95,7 +132,17 @@ CREATE TABLE IF NOT EXISTS `commande` (
   KEY `id_code_promo` (`id_code_promo`),
   KEY `id_adresse_livraison` (`id_adresse_livraison`),
   KEY `id_adresse_facturation` (`id_adresse_facturation`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id_commande`, `id_user`, `id_code_promo`, `date_commande`, `date_reglement`, `id_statut`, `id_adresse_livraison`, `id_adresse_facturation`) VALUES
+(1, 17, '20SUR20', '2023-07-17 16:18:18', '2023-07-11', 0, 1, 1),
+(2, 8, 'LOVEJS', '2023-07-17 16:18:18', '2023-07-16', 0, 7, 13),
+(3, 20, NULL, '2023-07-17 16:20:59', NULL, 0, 12, 12),
+(4, 6, 'JOYEUXANNIVMAXIME', '2023-07-17 16:20:59', '2023-07-17', 0, 9, 9);
 
 -- --------------------------------------------------------
 
@@ -149,7 +196,39 @@ CREATE TABLE IF NOT EXISTS `role` (
   `id_role` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   PRIMARY KEY (`id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id_role`, `nom`) VALUES
+(1, 'client'),
+(2, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statut_commande`
+--
+
+DROP TABLE IF EXISTS `statut_commande`;
+CREATE TABLE IF NOT EXISTS `statut_commande` (
+  `id_statut` int(11) NOT NULL AUTO_INCREMENT,
+  `statut` varchar(50) COLLATE utf16_bin NOT NULL,
+  PRIMARY KEY (`id_statut`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Déchargement des données de la table `statut_commande`
+--
+
+INSERT INTO `statut_commande` (`id_statut`, `statut`) VALUES
+(1, 'en attente de payement'),
+(2, 'en cours de préparation'),
+(3, 'préparée'),
+(4, 'envoyée'),
+(5, 'réceptionnée');
 
 -- --------------------------------------------------------
 
@@ -175,6 +254,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
+  `genre` enum('h','f') NOT NULL,
   `tel` varchar(15) NOT NULL,
   `date_naissance` date NOT NULL,
   `email` varchar(150) NOT NULL,
@@ -184,7 +264,33 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id_user`),
   KEY `id_adresse` (`id_adresse`),
   KEY `id_role` (`id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nom`, `prenom`, `genre`, `tel`, `date_naissance`, `email`, `mot_passe`, `id_adresse`, `id_role`) VALUES
+(1, 'Wade', 'Whilemina', 'h', '02 14 58 79 09', '1971-10-18', 'lacinia@protonmail.edu', 'EGD15HTY5FB', 4, 1),
+(2, 'Hutchinson', 'Cheryl', 'f', '03 27 37 20 41', '1982-04-13', 'sed.pede.nec@hotmail.net', 'ORM37ZVU4SC', 9, 1),
+(3, 'Rios', 'Caesar', 'f', '08 44 28 08 87', '1976-06-27', 'donec.consectetuer@aol.edu', 'LOA86RVM6LS', 16, 1),
+(4, 'Webb', 'Lewis', 'f', '03 89 38 31 29', '1949-11-12', 'integer.vulputate.risus@protonmail.net', 'JNC43UQM0HO', 14, 1),
+(5, 'Mcdowell', 'Scarlet', 'f', '03 96 97 28 91', '1969-04-25', 'orci.in@protonmail.ca', 'WEG12NNK7MM', 10, 1),
+(6, 'Bender', 'Elijah', 'f', '07 67 55 14 84', '1994-09-03', 'diam@aol.org', 'ETM42PML7IL', 12, 1),
+(7, 'Graham', 'Reed', 'h', '07 01 15 81 25', '1989-08-04', 'ut.sem@icloud.couk', 'PBD03XSB9CH', 18, 1),
+(8, 'Daugherty', 'Hope', 'f', '03 65 53 33 27', '1958-08-03', 'tellus.nunc.lectus@hotmail.ca', 'DGM78TUN4VH', 4, 1),
+(9, 'Spears', 'Edan', 'h', '02 64 60 51 25', '1960-07-08', 'sem.mollis@aol.edu', 'WLO44JVB1LX', 6, 1),
+(10, 'Velazquez', 'Mary', 'h', '06 57 61 25 31', '1967-01-13', 'et.magnis@protonmail.ca', 'YUH06OBM6IL', 11, 1),
+(11, 'Lindsey', 'Keiko', 'f', '02 21 81 56 93', '1968-04-17', 'phasellus.nulla@yahoo.com', 'RXX13EKW8CM', 17, 1),
+(12, 'Calhoun', 'Denton', 'h', '08 78 85 51 85', '1949-04-15', 'diam@protonmail.ca', 'SFR44OUH2NS', 7, 1),
+(13, 'Carey', 'Sonia', 'h', '05 34 27 91 84', '1963-07-18', 'sed.et.libero@icloud.edu', 'WCO40YUP8LU', 2, 1),
+(14, 'Mccarty', 'Catherine', 'f', '04 59 54 03 36', '1959-05-31', 'vel@icloud.com', 'VHW73YTF3VD', 6, 1),
+(15, 'Trevino', 'Jorden', 'h', '08 65 88 20 20', '1978-01-13', 'sed.dui@hotmail.com', 'NMJ31HDG0HC', 12, 1),
+(16, 'Garrett', 'Stephen', 'h', '07 74 53 77 71', '1967-07-31', 'at.lacus@yahoo.couk', 'EUR09IVR5UW', 4, 1),
+(17, 'Burke', 'Talon', 'f', '07 37 62 71 65', '1962-02-26', 'phasellus.in.felis@google.ca', 'RYK28BSJ0RP', 3, 1),
+(18, 'Velez', 'Imogene', 'f', '03 79 51 83 05', '1942-07-16', 'in.mi.pede@aol.ca', 'GDK91IMX4SK', 17, 1),
+(19, 'Wise', 'Alan', 'f', '03 35 32 74 74', '1987-06-04', 'montes.nascetur.ridiculus@protonmail.edu', 'GXA55RRH6JO', 3, 1),
+(20, 'Farley', 'Zeph', 'h', '01 33 27 12 93', '2008-11-23', 'consectetuer@protonmail.org', 'GJY31PNN8UD', 13, 1);
 
 --
 -- Contraintes pour les tables déchargées
