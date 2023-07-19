2d27e41c3a2955465f57567604c48e1954eaab86
Maxime Hinderschiette <maxhin94@gmail.com>
2023-07-19 16:32:31 +0200

    Fonction card statistiques

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -19,10 +19,41 @@ class AdminController extends AbstractController
     {
 
         // Obtenir le nombre de produits :
-        $req = "SELECT COUNT(*) FROM produit";
+        $req = "SELECT COUNT(*) as nbproduit 
+                FROM article";
         $statement = $this->pdo->prepare($req);
         $statement->execute();
         $context['produit'] = $statement->fetch(PDO::FETCH_ASSOC);
+
+        // Obtenir le nombre de produits :
+        $req = "SELECT COUNT(*) as nbcommande 
+                FROM commande
+                WHERE id_statut > 1";
+        $statement = $this->pdo->prepare($req);
+        $statement->execute();
+        $context['produit'] += $statement->fetch(PDO::FETCH_ASSOC);
+
+        // Obtenir le nombre de produits :
+        $req = "SELECT COUNT(*) as nbclient 
+                FROM user
+                WHERE id_role = 1";
+        $statement = $this->pdo->prepare($req);
+        $statement->execute();
+        $context['produit'] += $statement->fetch(PDO::FETCH_ASSOC);
+
+        $req = "SELECT (subquery.revenus - (subquery.revenus * subquery.remise/100)) as revenusnet
+            FROM (
+                SELECT SUM((a.prix - (a.prix * a.remise / 100)) * p.quantite) as revenus, cp.remise
+                FROM commande c 
+                JOIN panier p ON p.id_commande = c.id_commande
+                JOIN article a ON a.id_article = p.id_article
+                JOIN code_promo cp ON cp.id_code_promo = c.id_code_promo
+                WHERE c.id_statut > 1 
+                GROUP BY c.id_commande, cp.remise
+            ) subquery;";
+        $statement = $this->pdo->prepare($req);
+        $statement->execute();
+        $context['produit'] += $statement->fetch(PDO::FETCH_ASSOC);
 
         // Rendu du template Twig
         return $this->twig->render('admin/dashboard.twig', $context);
