581bb3159925634d02d11f5129dee04287ba8156
Olivier PERDRIX <116632018+operdrix@users.noreply.github.com>
2023-07-19 19:33:45 +0200

    Correction contexte titre des pages

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,8 +10,10 @@ class IndexController extends AbstractController
   #[Route(path: "/", name: "indexPage_page")]
   public function indexPage(): string
   {
+    $context['page'] = array(
+      'titre' => 'Emma Pierre - Votre pannier',
+    );
     // Rendu du template Twig
-    return $this->twig->render('index.html.twig');
+    return $this->twig->render('index.html.twig', $context);
   }
-
 }
