a2c3fb931c84db0bdb9997893618133034e2d200
Azroph <56892575+Azroph@users.noreply.github.com>
2023-07-20 15:02:06 +0200

    modif en cours

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,16 +8,16 @@ use App\Routing\Attribute\Route;
 class ContactController extends AbstractController
 {
 
-  #[Route(path: "/contact", name: "contact_page")]
-  public function showContact(): string
-  {
+    #[Route(path: "/contact", name: "contact_page")]
+    public function showContact(): string
+    {
 
-    // Contexte Twig
-    $context['page'] = array(
-      'titre' => 'Emma Pierre Contact - Bijoux en pierre',
-    );
+        // Contexte Twig
+        $context['page'] = array(
+            'titre' => 'Emma Pierre Contact - Bijoux en pierre',
+        );
 
-    // Rendu du template Twig
-    return $this->twig->render('contact.html.twig', $context);
-  }
+        // Rendu du template Twig
+        return $this->twig->render('contact.html.twig', $context);
+    }
 }
