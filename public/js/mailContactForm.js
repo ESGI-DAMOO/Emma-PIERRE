8acbdff8f759e7f343e96a4356391a5f2f5a2fb8
Azroph <56892575+Azroph@users.noreply.github.com>
2023-07-20 14:52:48 +0200

    Update mailContactForm.js

--- public/js/mailContactForm.js
+++ public/js/mailContactForm.js
@@ -1,16 +1,7 @@
-// Sélectionnez le formulaire et ajoutez un écouteur d'événements pour l'événement 'submit'
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    // empêche le comportement par défaut de l'événement submit, qui est de recharger la page
-    event.preventDefault();
+document.getElementById('submit').addEventListener('click', function(event) {
+// Empêche le comportement par défaut de l'événement de clic, qui est d'envoyer le formulaire
+event.preventDefault();
   
-    // récupérez les données du formulaire
-    var nom = document.querySelector('.nom').value;
-    var prenom = document.querySelector('.prenom').value;
-    var email = document.querySelector('.email').value;
-    var objet = document.querySelector('.objet').value;
-    var message = document.querySelector('textarea[name="message"]').value;
-  
-    // Faire quelque chose avec les données, par exemple afficher dans la console
-    console.log('Nom: ' + nom + ', Prénom: ' + prenom + ', Email: ' + email + ', Objet: ' + objet + ', Message: ' + message);
-  });
-  
+// Ajoute le message de confirmation d'envoie
+document.getElementById('message').textContent = 'Merci, votre message à bien été envoyé';
+});
